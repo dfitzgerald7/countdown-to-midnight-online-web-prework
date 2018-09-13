d43fd31e92ed87fff79263d125279ca702d34545
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-13 18:06:27 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,11 @@ def countdown(seconds)
   end 
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+  while seconds >= 0 
+    puts "#{seconds} SECONDS(S)!"
+    seconds -= 1 
+  end 
+  "HAPPY NEW YEAR!"
+end
